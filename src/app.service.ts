26541b1420c2346d41b75696087b771f99238612
Daniel Okoronkwo <danielokoronkwo90@gmail.com>
2023-10-02 15:10:05 +0100

    chore: fix database credentials

--- src/app.service.ts
+++ src/app.service.ts
@@ -6,3 +6,6 @@ export class AppService {
     return 'Hello World Test!';
   }
 }
+
+//ON APP: postgres://yblyakxplkpjgy:e7f2e8a253c173bc109541df2a43ca05569720249d3dc3d4fe6ae79d2df61d65@ec2-44-214-132-149.compute-1.amazonaws.com:5432/d8psme9isosol7
+// Actual: postgres://yblyakxplkpjgy:e7f2e8a253c173bc109541df2a43ca05569720249d3dc3d4fe6ae79d2df61d65@ec2-44-214-132-149.compute-1.amazonaws.com:5432/d8psme9isosol7
